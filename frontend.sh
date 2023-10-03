e36a0fdae847e27b149ea5add62df2851a5ac3ff
Venktesh Sharma <svenktesh77@gmail.com>
2023-10-03 16:11:30 -0500

    Phase 1

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,14 @@
 dnf install nginx -y
+cp expense.conf /etc/nginx/default.dexpense.conf
 
 systemctl enable nginx
 systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-cp expense.conf /etc/nginx/default.dexpense.conf
-
 
 systemctl restart nginx
 
